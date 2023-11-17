f60fcb28e67fe670a890bc935471b5e9262d1be0
Nikolaos Lichnaropoylos <nicklich2227@gmail.com>
2023-11-17 18:24:24 +0200

    c2 - Read and display 5 items from data.txt

--- rainfall.f95
+++ rainfall.f95
@@ -2,4 +2,32 @@ program rainfall
 
 implicit none
 
+
+integer::i
+integer::rain(5)
+character(20)::town(5)
+
+
+open(10,file='data.txt',status='old')
+
+
+ read(10,*)
+
+
+    do i=1,5
+      read(10,*) town(i) , rain(i)
+    end do
+
+
+close(10)
+
+
+     print*, "Town","                    Average Yearly Rainfall (in millimeters)"
+ 
+ 
+do i=1,5
+print*,town(i),rain(i)
+end do
+
+
 end program
